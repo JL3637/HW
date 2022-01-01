560fa8efddc7840ac40e6c83dce8b44360c94823
jason <jason36375667@gmail.com>
2022-01-01 19:20:53 +0800

    test

--- hw.py
+++ hw.py
@@ -5,7 +5,7 @@ class Judge:
         answer: (int) the final answer
         """
         self.answer = answer
-        # TODO
+
 
     def guess(self, num: str) -> bool:
         """
@@ -15,24 +15,19 @@ class Judge:
         num: the number that it guessed
         return: whether the player guess the correct answer
         """
-        a = 0
-        b = 0
-        s1 = list(num)
-        s2 = list(self.answer)
-        S1 = list(map(int, s1))
-        S2 = list(map(int, s2))
-        for i in range(0,len(num)):
-            for j in range(0,len(num)):
-                if S1[j] == S2[i] and i == j:
-                    a += 1
-                elif S1[j] == S2[i]:
-                    b += 1
-        print(f"Your guess is {num}; the result is {a}A{b}B")
-        if a == len(num):
-            return True
-        else:
-            return False
-        # TODO
+        count_A = 0
+        count_B = 0
+        for i in range(len(num)):
+            if num[i] == self.answer[i]:
+                count_A += 1
+            for j in range(len(self.answer)):
+                if i == j:
+                    continue
+                if num[i] == self.answer[j]:
+                    count_B += 1
+        s = 'Your guess is ' + str(num) + '; the result is ' + str(count_A) + 'A' + str(count_B) + 'B'
+        print(s)
+        return count_A == len(num)
 
 
 def read_input(guess_len: int) -> str:
@@ -47,21 +42,31 @@ def read_input(guess_len: int) -> str:
         "Invalid, please enter your guess again:\n"
     Note: a valid guess means contain only guess_len non-repetitive integer range from 0~9
     """
-    print("Enter your guess:")
+    reply = input("Enter your guess:\n")
     while True:
-        my_guess = input()
-        list1 = list(my_guess)
-        if len(my_guess) == guess_len and my_guess.isdigit() and len(set(list1)) == len(list1):
-            return my_guess
-        else:
-            print("Invalid, please enter your guess again:")
-    # TODO
+        check = 0
+        if len(reply) != guess_len:
+            reply = input("Invalid, please enter your guess again:\n")
+            continue
+        for i in range(guess_len):
+            if str.isdigit(reply) == False:
+                check = 1
+                break
+            for j in range(guess_len):
+                if j == i:
+                    continue
+                if reply[i] == reply[j]:
+                    check = 1
+        if check == 1:
+            reply = input("Invalid, please enter your guess again:\n")
+            continue
+        break
+    return reply
+
 
 
 def enter_answer() -> str:
     """
     Function that enter the answer, you can assume that the answer must be valid.
     """
-    s = input()
-    return s
-    # TODO
+    return input()
